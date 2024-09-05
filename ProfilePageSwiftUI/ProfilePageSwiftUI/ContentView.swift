b8910f782642fcc48503d549bcd51ee7dc99f8f7
Harish Kshirsagar <harishkshirsagar@Harishs-MacBook-Air.local>
2024-09-05 15:29:11 +0530

    Profile page

---
+++ ProfilePageSwiftUI/ProfilePageSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProfilePageSwiftUI
+//
+//  Created by Harish Kshirsagar on 05/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProfilePageSwiftUI/ProfilePageSwiftUI/ProfilePageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfilePageSwiftUIApp.swift
+//  ProfilePageSwiftUI
+//
+//  Created by Harish Kshirsagar on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ProfilePageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
